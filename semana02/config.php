1fa941f1fce472eae303772d9d66eba1d2482559
Karoline Goergen <goergenkarol@gmail.com>
2022-06-02 22:01:58 -0300

    Segunda parte

---
+++ semana02/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    class config {
+
+        // Nome do DB
+        public const DB_NOME = 'mysql';
+        // Usuário do DB
+        public const DB_USUARIO = '';
+        // Senha do DB
+        public const DB_SENHA = '';
+        // Charset da conexão PDO
+        public const DB_CHARSET = 'utf8';
+    }
